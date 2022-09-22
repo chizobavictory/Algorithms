562cabd9d0ffa46308483064e0f2de22e28d84f5
Chizoba Victory <chizoba.victory1@gmail.com>
2022-09-22 13:55:26 +0100

    Update factorial.js

--- factorial.js
+++ factorial.js
@@ -1,10 +1,10 @@
 /* When you factorialize a number, you are multiplying that number by each consecutive number minus one.
-For example, factorializing 5 is 5 * 4 * 3 * 2 * 1 = 120. 
+For example, factorials of 5 is 5 * 4 * 3 * 2 * 1 = 120. 
 Create a function that returns the factorial of number passed as argument */
 
-function factorialize(num) {
+function factorial(num) {
   if(num < 0) return -1
   else if(num === 0) return 1
-  else{num * factorialize(num - 1)}
+  else{num * factorial(num - 1)}
 
 // console.log(factorialize(5)) => 120
