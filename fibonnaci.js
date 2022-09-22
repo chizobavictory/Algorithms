e9a7b7a56a8bdd743cab7c7727a804fc22930027
Chizoba Victory <chizoba.victory1@gmail.com>
2022-09-22 10:10:11 +0100

    Update fibonnaci.js

--- fibonnaci.js
+++ fibonnaci.js
@@ -1,4 +1,4 @@
-/*The Fibonacci sequence is the integer sequence where the first two terms are 0 and 1. After that, the next term is defined as the sum of the previous two terms.*/
+/*The Fibonacci sequence is the integer sequence where the first two terms are 0 and 1. After that, the next term is defined as the sum of all previous terms.*/
 
 function fibonnaci(num){
     if(num <= 1) return 1;
